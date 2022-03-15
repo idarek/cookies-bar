f749b31b2e0720588c2c6d26408ff13862ddbf64
Dariusz Więckiewicz <dariusz@wieckiewicz.org>
2022-03-15 13:26:42 +0000

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,6 +1,8 @@
 if (localStorage.getItem("cookieBannerDisplayed")) {
   document.querySelector('.js-cookie-banner').remove();
 } else {
+  
+  document.getElementById("CookieBanner").style.display = "block";
 
 function dismiss() {
       document.querySelector('.js-cookie-banner').remove();
